f78636511096e7be6f09356a92b9246b3ee059f8
Kunal Shroff <shroffk@bnl.gov>
2019-02-13 11:10:01 -0500

    Update install_jvm_env.sh

--- setup/install_jvm_env.sh
+++ setup/install_jvm_env.sh
@@ -2,12 +2,10 @@
 
 # install jdk 11
 wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz -O /tmp/openjdk-11+28_linux-x64_bin.tar.gz
-tar xfvz /tmp/openjdk-11+28_linux-x64_bin.tar.gz --directory /usr/lib/jvm
-export JAVA_HOME=/usr/lib/jvm/jdk-11.0.1
-export PATH="$JAVA_HOME/bin:$PATH"
+mkdir -p /opt/lib/jvm
+tar xfvz /tmp/openjdk-11+28_linux-x64_bin.tar.gz --directory /opt/lib/jvm
 
 # install maven
 wget http://mirror.cc.columbia.edu/pub/software/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz -O /tmp/apache-maven-3.6.0-bin.tar.gz
-tar xzvf /tmp/apache-maven-3.6.0-bin.tar.gz --directory /usr/lib
-export MVN_HOME=/usr/lib/apache-maven-3.6.0
-export PATH="$MVN_HOME/bin:$PATH"
+tar xzvf /tmp/apache-maven-3.6.0-bin.tar.gz --directory /opt/lib
+  
