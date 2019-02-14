a5c98fb58bd33754f7a1e37e1a6816743f007f99
Kunal Shroff <shroffk@bnl.gov>
2019-02-14 15:21:36 -0500

    Delete configure.sh

--- configure/configure.sh
+++
@@ -1,9 +0,0 @@
-# In the past, a file like this contained all the settings.
-#
-# To simplify maintenance, that has been split into
-# several profile.d/*.sh
-# which are all parsed here in alphabetical order
-for i in profile.d/*.sh
-do
-    source $i
-done
