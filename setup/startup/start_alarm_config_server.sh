f250dccf0f510c2a8e251a91a6fd1b568bb58411
Kunal Shroff <shroffk@bnl.gov>
2019-02-15 15:47:34 -0500

    Create start_alarm_config_server.sh

---
+++ setup/startup/start_alarm_config_server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export JAVA_HOME=/home/train/epics-tools/lib/jvm/jdk-11.0.2
+export PATH="$JAVA_HOME/bin:$PATH"
+
+V="0.0.1-SNAPSHOT"
+
+JAR="service-alarm-config-logger-0.0.1-SNAPSHOT.jar"
+
+cd /home/train/epics-tools/clients/phoebus/services/alarm-config-logger/target/
+java -jar $JAR "$@"
