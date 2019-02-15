4c26b106787bb6fee7b6b32e66f61182811adc85
Kunal Shroff <shroffk@bnl.gov>
2019-02-15 10:14:01 -0500

    Create start_phoebus.sh

---
+++ setup/startup/start_phoebus.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+export JAVA_HOME=~/epics-tools/lib/jvm/jdk-11.0.2
+export PATH="$JAVA_HOME/bin:$PATH"
+
+export MVN_HOME=~/epics-tools/lib/apache-maven-3.6.0
+export PATH="$MVN_HOME/bin:$PATH"
+
+cd /home/train/epics-tools/clients/phoebus/phoebus-product/target
+
+
+JAR=product-0.0.1-SNAPSHOT.jar
+
+OPT="-settings /home/train/epics-tools/setup/settings/phoebus.ini"
+
+# To get one instance, use server mode
+#OPT="$OPT -server 4918"
+
+JDK_JAVA_OPTIONS=" -DCA_DISABLE_REPEATER=true"
+JDK_JAVA_OPTIONS+=" -Dnashorn.args=--no-deprecation-warning"
+JDK_JAVA_OPTIONS+=" -Djdk.gtk.verbose=false -Djdk.gtk.version=2  -Dprism.forceGPU=true"
+JDK_JAVA_OPTIONS+=" -Dlogback.configurationFile=/home/train/epics-tools/setup/settings/logback.xml"
+JDK_JAVA_OPTIONS+=" -Dorg.csstudio.javafx.rtplot.update_counter=false"
+export JDK_JAVA_OPTIONS
+
+java -jar $JAR $OPT $SETTINGS "$@" &
